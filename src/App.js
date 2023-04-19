737700c8d954f5d90fec098165c6d25ac1435d2e
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-19 12:33:08 -0400

    set up router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import RouteSwitch from './components/router/RouteSwitch';
+
 import './styles/App.css';
 
 function App() {
   return (
     <div className="app">
+      <RouteSwitch />
     </div>
   );
 }

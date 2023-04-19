56c33c97641a5d041cf1805762e3d8875577606a
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-19 16:11:06 -0400

    add all required fields to post form

--- src/components/forms/PostForm.js
+++ src/components/forms/PostForm.js
@@ -1,18 +1,59 @@
 import React, { useRef, useState } from 'react';
 import { PropTypes as PT } from 'prop-types';
 import { Editor } from '@tinymce/tinymce-react';
+import { useNavigate } from 'react-router-dom';
 
 import tinymceConfig from '../../config/tinyMCE';
+import { sendReq, getStorageAuth } from '../../utils/helpers';
 
 function PostForm({ post }) {
   const editorRef = useRef(null);
 
+  const { user, token } = getStorageAuth();
+
   const [title, setTitle] = useState(post ? post.title : '');
+  const [text, setText] = useState(post);
   const [tags, setTags] = useState(post ? post.tags : []);
+  const [published, setPublished] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('');
 
-  const handleSubmit = e => {
+  const navigate = useNavigate();
+
+  const handleChange = content => setText(content);
+
+  const handleSubmit = async e => {
     e.preventDefault();
+    try {
+      if (text.trim().length === 0) {
+        setErrorMsg('Content must not be empty!');
+        return;
+      }
 
+      const res = await sendReq(
+        post ? 'PUT' : 'POST',
+        post ? `posts/${post._id}` : 'posts',
+        {
+          author: user._id,
+          title, text, tags, published,
+          createdAt: new Date(),
+        },
+        token,
+      );
+
+      const json = await res.json();
+
+      if (res.status === 200) {
+        navigate('/');
+      } else {
+        const message = json.error
+          ? json.error.message
+          : json.errors.map(err => err.msg).join(' ');
+        
+        setErrorMsg(message);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -25,10 +66,14 @@ function PostForm({ post }) {
         onChange={e => setTitle(e.target.value)}
         required
       />
+      <p className="form-error">{errorMsg}</p>
       <Editor
-        apiKey=""
+        apiKey={process.env.REACT_APP_TINYMCE_API_KEY}
         onInit={(e, editor) => editorRef.current = editor}
         init={tinymceConfig}
+        value={text}
+        onEditorChange={handleChange}
+        required
       />
       <label htmlFor="tags">Tags</label>
       <input
@@ -36,6 +81,14 @@ function PostForm({ post }) {
         id="tags"
         value={tags.join(' ')}
         onChange={e => setTags(e.target.value.split(' '))}
+        placeholder="food cooking healthy"
+      />
+      <label htmlFor="published">Publish?</label>
+      <input
+        type="checkbox"
+        id="published"
+        checked={published}
+        onChange={() => setPublished(prev => !prev)}
       />
       <button type="Submit">{post ? 'Update' : 'Create' }</button>
     </form>
@@ -44,6 +97,7 @@ function PostForm({ post }) {
 
 PostForm.propTypes = {
   post: PT.shape({
+    _id: PT.string.isRequired,
     title: PT.string.isRequired,
     text: PT.string.isRequired,
     tags: PT.arrayOf(PT.string),

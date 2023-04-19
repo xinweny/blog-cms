bba2c42562a2e2838fe8a681ee171bfd6e58e0ce
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-19 12:34:08 -0400

    init pages

---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DashboardPage() {
+  return (
+    <main>
+      <p>Dashboard</p>
+    </main>
+  );
+}
+
+export default DashboardPage;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <main>
+      <p>Home</p>
+    </main>
+  );
+}
+
+export default LandingPage;

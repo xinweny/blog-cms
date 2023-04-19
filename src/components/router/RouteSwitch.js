a193812304c37ef35a952c5b1d95c5162b43d4b3
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-19 17:04:31 -0400

    update edit route

--- src/components/router/RouteSwitch.js
+++ src/components/router/RouteSwitch.js
@@ -25,7 +25,7 @@ function RouteSwitch() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/posts/new" element={<PostFormPage />} />
-        <Route path="/posts/:id" element={<PostFormPage />} />
+        <Route path="/posts/:id/edit" element={<PostFormPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>

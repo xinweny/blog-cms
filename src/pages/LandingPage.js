f3cccc8764232f609c239c2a6f11c691257e1419
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-19 12:55:50 -0400

    add basic landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function LandingPage() {
   return (
     <main>
-      <p>Home</p>
+      <div>
+        <h2>BlogTree</h2>
+        <p>Introducing BlogTree CMS, the easiest way to create and manage your stories.</p>
+        <br />
+        <p>With BlogTree, you can write and manage your articles in a few clicks. Our easy-to-use platform allows you to create and customize posts, and engage with your audience. Start writing and inspire your audience today.</p>
+        <Link to="/signup">Unleash Your Creativity</Link>
+      </div>
     </main>
   );
 }

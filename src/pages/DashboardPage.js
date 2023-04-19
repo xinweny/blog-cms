bd12e18c4284dd5ee41dc07ba07d0c674205a7a3
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-19 13:51:23 -0400

    set up dashboard page

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import useFetch from '../hooks/useFetch';
+import { getStorageAuth } from '../utils/helpers';
+
+import SideNav from '../components/ui/SideNav';
+import PostCard from '../components/ui/PostCard';
+
 function DashboardPage() {
+  const { user } = getStorageAuth();
+  
+  const [posts] = useFetch(`users/${user.id}/posts`, []);
+
   return (
     <main>
-      <p>Dashboard</p>
+      <SideNav />
+      <h3>Articles</h3>
+      {posts.length > 0
+        ? posts.map(post => (
+          <PostCard key={post._id} post={post} />
+        ))
+        : <p>No articles yet. Write one now!</p>}
+      <h3>Comments</h3>
     </main>
   );
 }
